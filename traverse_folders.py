d715fd0292227a9420b0e67dcb0fcb8edcaf8fe0
Utkarsh Kashyap <utkarsh.k05@gmail.com>
2017-03-24 15:52:42 +0530

    IDDFS file search

---
+++ traverse_folders.py
@@ -0,0 +1,68 @@
+from os import listdir
+from os.path import isfile, join, isdir
+import sys
+
+def dfs_directory(root, filename, depth, formatstring):
+    """ Fuction to perform DFS on Directory structure
+        params: root: Path of the current subdirectory
+                filename: Name of the file to be searched
+                depth: Remaining depth to be explored
+                formatstring: For pretty output
+    """
+
+    """Terminate search if depth is exceeded"""
+    if depth < 0:
+        return None, False, depth
+
+    files = [f for f in listdir(root) if isfile(join(root, f))]
+    files.sort()
+
+    for f in files:
+        print formatstring + "File: " + f
+
+    """Terminal test: Test all the files for checking whether the reuqired file is found."""
+    for f in files:
+        if f == filename:
+            return join(root, f), True, depth
+
+    directories = [d for d in listdir(root) if isdir(join(root, d))]
+    directories.sort()
+
+    for d in directories:
+        new_path = join(root, d)
+        print formatstring + "Folder: " + d
+        file_path, result, file_depth = dfs_directory(new_path, filename, depth - 1, "|   " + formatstring)
+        if result == True:
+            return file_path, result, file_depth
+
+    return None, False, depth
+
+def iddfs_directory(root, filename, depth, formatstring):
+    """ Fuction to perform ID-DFS on Directory structure
+        params: root: Path of the current subdirectory
+                filename: Name of the file to be searched
+                depth: Remaining depth to be explored
+                formatstring: For pretty output
+    """
+
+    for current_depth in xrange(depth+1):
+        print "\nCurrent depth cut-off %d\n" % (current_depth)
+        print "Root Directory: %s" % (root_path)
+        file_path, result, file_depth = dfs_directory(root_path, search_name, current_depth, formatstring)
+        if result == True:
+            return file_path, result, current_depth
+
+    return None, False, depth
+
+if __name__ == '__main__':
+    argument_list = sys.argv
+    search_depth  = int(argument_list[1])
+    search_name = argument_list[2]
+    root_path = argument_list[3]
+    formatstring = "|-- "
+
+    file_path, result, file_depth = iddfs_directory(root_path, search_name, search_depth, formatstring)
+    if result == False:
+        print "\nNo file found till folder depth %d" % (search_depth)
+    else:
+        print "\nFile found at path \"%s\" and depth %d\n" % (file_path, file_depth)
